52a3d4935c6478da47a8632bb07c409a69a0f2db
Ericlys Moreira <ericlysm@gmail.com>
2022-12-16 17:28:41 -0300

    feat: prisma repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,7 +1,8 @@
+import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
-  //static - because i don`t need to instantiate it
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -11,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createAt,
+      },
+      raw.id,
+    );
   }
 }
