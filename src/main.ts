2cfbf5ad2d4b8c8a80601f02146b11deb8288a7d
Ericlys Moreira <ericlysm@gmail.com>
2022-12-16 23:13:55 -0300

    feat: messaging consummer with kafka

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  //app.get - dependency injection
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+
+  app.startAllMicroservices();
 
   await app.listen(3000);
 }
